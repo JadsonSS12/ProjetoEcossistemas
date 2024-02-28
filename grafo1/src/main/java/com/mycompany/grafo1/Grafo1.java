c74f7a36a95acc6425f3ea307ff8e2bd7b0d2cf8
Jadson de Souza Silva <jadson.w123@gmail.com>
2024-02-28 14:40:37 -0300

    esquema de grafo

---
+++ grafo1/src/main/java/com/mycompany/grafo1/Grafo1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.grafo1;
+
+/**
+ *
+ * @author Usuário
+ */
+public class Grafo1 {
+
+    public static void main(String[] args) {
+        Grf<String> grf = new Grf<String>();
+        
+        grf.adicionarVertice("dado");
+    }
+}
---
+++ grafo1/src/main/java/com/mycompany/grafo1/Vertice.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.grafo1;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Usuário
+ * @param <TIPO>
+ */
+public class Vertice<TIPO> {
+    private TIPO dado;
+    private ArrayList<Aresta<TIPO>> arestasEntrada;
+    private ArrayList <Aresta<TIPO>> arestasSaida;
+    
+    public Vertice(TIPO valor){
+        this.dado = valor;
+        this.arestasEntrada = new ArrayList<Aresta<TIPO>> ();
+        this.arestasSaida = new ArrayList<Aresta<TIPO>>  ();
+    }
+
+    public TIPO getDado() {
+        return dado;
+    }
+
+    public void setDado(TIPO dado) {
+        this.dado = dado;
+    }
+
+    public ArrayList<Aresta<TIPO>> getArestasEntrada() {
+        return arestasEntrada;
+    }
+
+    public void setArestasEntrada(ArrayList<Aresta<TIPO>> arestasEntrada) {
+        this.arestasEntrada = arestasEntrada;
+    }
+
+    public ArrayList<Aresta<TIPO>> getArestasSaida() {
+        return arestasSaida;
+    }
+
+    public void setArestasSaida(ArrayList<Aresta<TIPO>> arestasSaida) {
+        this.arestasSaida = arestasSaida;
+    }
+
+    
+    
+    public void adicionarArestaEntrada(Aresta<TIPO> aresta){
+        this.arestasEntrada.add(aresta);
+    }
+    
+    public void adicionarArestaSaida(Aresta<TIPO> aresta){
+        this.arestasSaida.add(aresta);
+    }
+
+}
